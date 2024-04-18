3dda4f032f1c66c06aa096f2c90d2be445d95452
Omeiza Alabi <omeizaalabi@gmail.com>
2024-04-18 10:05:17 +0100

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:injectable/injectable.dart';
 
 void main() async {
   // Load Dotenv file
